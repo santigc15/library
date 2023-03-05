fe703c9d39c6998b27054f1aba608a899ddd2de6
santigc15 <gomezcacheiro@gmail.com>
2023-03-05 23:44:46 +0100

    nada

--- main.php
+++ main.php
@@ -1,1 +1,22 @@
-<h2>Principal</h2>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/main.css">
+    <title>Document</title>
+</head>
+
+<body>
+
+
+
+
+
+</header>
+
+</body>
+
+</html>
